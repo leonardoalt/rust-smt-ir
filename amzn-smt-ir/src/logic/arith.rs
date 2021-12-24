28cc76760e659cceef36148fa81c6ab017089327
Leo Alt <leo@ethereum.org>
2021-12-25 00:34:00 +0100

    Overload some arithmetic operators

--- amzn-smt-ir/src/logic/arith.rs
+++ amzn-smt-ir/src/logic/arith.rs
@@ -1,10 +1,12 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 use crate::{
-    fold::Fold, visit::Visit, Ctx, IIndex, IOp, ISort, Logic, Operation, QualIdentifier, Sorted,
+    logic::ALL, fold::Fold, visit::Visit, Ctx, IIndex, IOp, ISort, Logic, Operation, QualIdentifier, Sorted,
     Term, UnknownSort, Void, UF,
 };
 use smallvec::SmallVec;
+
+use std::ops;
 
 type Args<T> = SmallVec<[T; 2]>;
 
@@ -108,4 +110,138 @@ impl Logic for QF_LIA {
     type Op = LIAOp<Term<Self>>;
     type Quantifier = Void;
     type UninterpretedFunc = Void;
+}
+
+impl ops::Add for Term<ALL> {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::OtherOp(ArithOp::Plus([self, rhs].into()).into())
+    }
+}
+
+impl ops::Sub for Term<ALL> {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::OtherOp(ArithOp::Minus([self, rhs].into()).into())
+    }
+}
+
+impl ops::Neg for Term<ALL> {
+    type Output = Self;
+    fn neg(self) -> Self::Output {
+        Self::OtherOp(ArithOp::Neg(self).into())
+    }
+}
+
+impl ops::Mul for Term<ALL> {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self::OtherOp(ArithOp::Mul([self, rhs].into()).into())
+    }
+}
+
+impl ops::Div for Term<ALL> {
+    type Output = Self;
+    fn div(self, rhs: Self) -> Self::Output {
+        Self::OtherOp(ArithOp::IntDiv([self, rhs].into()).into())
+    }
+}
+
+impl ops::Rem for Term<ALL> {
+    type Output = Self;
+    fn rem(self, rhs: Self) -> Self::Output {
+        Self::OtherOp(ArithOp::Mod(self, rhs).into())
+    }
+}
+
+#[test]
+fn add_overload() {
+    use smt2parser::concrete::Constant;
+
+    let zero = || Term::Constant(Constant::Numeral(0u8.into()).into());
+    assert_eq!((zero() + zero()).to_string(), "(+ 0 0)");
+
+    let x = Term::Variable("x".into());
+    let y = Term::Variable("y".into());
+    let z = x.clone() + y.clone();
+    assert_eq!(z.to_string(), "(+ x y)");
+
+    let z = x + y + zero();
+    assert_eq!(z.to_string(), "(+ (+ x y) 0)");
+}
+
+#[test]
+fn sub_overload() {
+    use smt2parser::concrete::Constant;
+
+    let zero = || Term::Constant(Constant::Numeral(0u8.into()).into());
+    assert_eq!((zero() - zero()).to_string(), "(- 0 0)");
+
+    let x = Term::Variable("x".into());
+    let y = Term::Variable("y".into());
+    let z = x.clone() - y.clone();
+    assert_eq!(z.to_string(), "(- x y)");
+
+    let z = x - y - zero();
+    assert_eq!(z.to_string(), "(- (- x y) 0)");
+}
+
+#[test]
+fn neg_overload() {
+    use smt2parser::concrete::Constant;
+
+    let zero = || Term::Constant(Constant::Numeral(0u8.into()).into());
+    assert_eq!((- zero()).to_string(), "(- 0)");
+
+    let x = Term::Variable("x".into());
+    let z = - x.clone();
+    assert_eq!(z.to_string(), "(- x)");
+}
+
+#[test]
+fn mul_overload() {
+    use smt2parser::concrete::Constant;
+
+    let zero = || Term::Constant(Constant::Numeral(0u8.into()).into());
+    assert_eq!((zero() * zero()).to_string(), "(* 0 0)");
+
+    let x = Term::Variable("x".into());
+    let y = Term::Variable("y".into());
+    let z = x.clone() * y.clone();
+    assert_eq!(z.to_string(), "(* x y)");
+
+    let z = x * y * zero();
+    assert_eq!(z.to_string(), "(* (* x y) 0)");
+}
+
+#[test]
+fn div_overload() {
+    use smt2parser::concrete::Constant;
+
+    let zero = || Term::Constant(Constant::Numeral(0u8.into()).into());
+    assert_eq!((zero() / zero()).to_string(), "(div 0 0)");
+
+    let x = Term::Variable("x".into());
+    let y = Term::Variable("y".into());
+    let z = x.clone() / y.clone();
+    assert_eq!(z.to_string(), "(div x y)");
+
+    let z = x / y / zero();
+    assert_eq!(z.to_string(), "(div (div x y) 0)");
+}
+
+#[test]
+fn mod_overload() {
+    use smt2parser::concrete::Constant;
+
+    let zero = || Term::Constant(Constant::Numeral(0u8.into()).into());
+    assert_eq!((zero() % zero()).to_string(), "(mod 0 0)");
+
+    let x = Term::Variable("x".into());
+    let y = Term::Variable("y".into());
+    let z = x.clone() % y.clone();
+    assert_eq!(z.to_string(), "(mod x y)");
+
+    let z = x % y % zero();
+    assert_eq!(z.to_string(), "(mod (mod x y) 0)");
 }
